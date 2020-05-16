8197197addf8959451ff062a51b8c61f99c883b9
Nagendra Patel <55997071+patelnagendra1@users.noreply.github.com>
2020-05-16 08:08:45 +0530

    Add files via upload

---
+++ controller/todoController.js
@@ -0,0 +1,32 @@
+var bodyParser = require('body-parser')
+
+var mangoose = require('mangoose')
+
+var data = [{item: 'Codechef contest'}, {item: 'Codeforces contest'}, {item: 'Any Assignment Submition'}]
+
+var urlencodedParser = bodyParser.urlencoded({extended: false})
+
+module.exports = function(app){
+
+app.get('/todo',function(req,res){
+
+  res.render('todo',{todos:data})
+})
+
+app.post('/todo', urlencodedParser, function(req,res){
+
+	data.push(req.body)
+	res.json(data)
+})
+
+
+app.delete('/todo/:item',function(req,res){
+
+	data = data.filter(function(todo){
+		return todo.item.replace(/ /g, '-') !== req.params.item
+	})
+	res.json(data)
+
+})
+
+}
